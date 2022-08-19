7c47d45d7dab730db6f3395ca7ce35afae694fd5
jepetolee <55366212+jepetolee@users.noreply.github.com>
2022-08-19 17:12:15 +0900

    Update MakeCsv2Img.py
    Added KDJ Indicator

--- stag/DatasetBuilder/MakeCsv2Img.py
+++ stag/DatasetBuilder/MakeCsv2Img.py
@@ -30,13 +30,13 @@ def ImagedataSetBuilder(url):
 
 def BuildNSaveImage(numpy_data, image_link):
     # build grid spec pyplot
-    chart_figure = plt.figure(40, 40)
-    chart_grid = gridspec.GridSpec(4, 4)
+    chart_figure = plt.figure(50, 40)
+    chart_grid = gridspec.GridSpec(5, 4)
     chart_figure.subplots_adjust(wspace=0.2, hspace=0.2)
 
     axes = list()
     axes.append(plt.subplot(chart_grid[0, :]))
-    for i in range(3):
+    for i in range(4):
         axes.append(plt.subplot(chart_grid[i + 1, :], sharex=axes[0]))
 
     '''
@@ -45,36 +45,49 @@ def BuildNSaveImage(numpy_data, image_link):
       2. Volumes 
       3. Relative Strength Indicator (14)
       4. Bollinger Band (20 2)
+      5. Stochastic Indicator (9,3,3)
     '''
 
     candlestick2_ohlc(axes[0], numpy_data['OpenPrice'], numpy_data['HighPrice'], numpy_data['LowPrice'],
                       numpy_data['ClosePrice'], width=1, colorup='r', colordown='b')
-    numpy_copies = numpy_data['ClosePrice'].copy()
+    ClosePrices = numpy_data['ClosePrice'].copy()
 
-    TwentyMovingAverage = numpy_copies.rolling(window=20).mean()
+    TwentyMovingAverage = ClosePrices.rolling(window=20).mean()
     axes[0].plot(numpy_data.index, TwentyMovingAverage, label='Moving Average 20')
-    axes[0].plot(numpy_data.index, numpy_copies.rolling(window=50).mean(), label='Moving Average 50')
-    axes[0].plot(numpy_data.index, numpy_copies.rolling(window=100).mean(), label='Moving Average 100')
-    axes[0].plot(numpy_data.index, numpy_copies.rolling(window=200).mean(), label='Moving Average 200')
+    axes[0].plot(numpy_data.index, ClosePrices.rolling(window=50).mean(), label='Moving Average 50')
+    axes[0].plot(numpy_data.index, ClosePrices.rolling(window=100).mean(), label='Moving Average 100')
+    axes[0].plot(numpy_data.index, ClosePrices.rolling(window=200).mean(), label='Moving Average 200')
 
     axes[1].bar(numpy_data.index, numpy_data['Volume'], color='k', width=0.8, align='center')
 
     candlestick2_ohlc(axes[2], numpy_data['OpenPrice'], numpy_data['HighPrice'], numpy_data['LowPrice'],
                       numpy_data['ClosePrice'], width=1, colorup='r', colordown='b')
-    TwentyStandardDeviation = numpy_copies.rolling(window=20).std()
+    TwentyStandardDeviation = ClosePrices.rolling(window=20).std()
     BollingerBandUpper = TwentyMovingAverage + 2 * TwentyStandardDeviation
     BollingerBandLower = TwentyMovingAverage - 2 * TwentyStandardDeviation
-    axes[2].plot(numpy_data.index, TwentyMovingAverage, label='Moving Average 20')
-    axes[2].plot(numpy_data.index, BollingerBandUpper, label='BollingerBandUpper 20 +2')
-    axes[2].plot(numpy_data.index, BollingerBandLower, label='BollingerBandLower 20 -2')
+    axes[2].plot(numpy_data.index, TwentyMovingAverage, 'r', label='Moving Average 20')
+    axes[2].plot(numpy_data.index, BollingerBandUpper, 'g', label='BollingerBandUpper 20 +2')
+    axes[2].plot(numpy_data.index, BollingerBandLower, 'b', label='BollingerBandLower 20 -2')
 
-    variance = numpy_copies - numpy_copies.shift(1)
+    variance = ClosePrices - ClosePrices.shift(1)
     rise_width = variance.where(variance > 0, 0)
     degrade_width = variance.where(variance < 0, 0)
     AverageUp = rise_width.ewm(aplha=1 / 14, min_periods=14).mean()
     AverageDown = degrade_width.ewm(aplha=1 / 14, min_periods=14).mean()
     RSI = AverageUp / (AverageUp + AverageDown) * 100
     axes[3].bar(numpy_data.index, RSI, label='RSI 14')
+
+    HighPrices = numpy_data['HighPrice'].copy()
+    HighInDays = HighPrices.rolling(window=9, min_periods=1).max()
+    LowPrices = numpy_data['LowPrice'].copy()
+    LowInDays = LowPrices.rolling(window=9, min_periods=1).max()
+    PeriodRSV = 100 * (ClosePrices - LowPrices) / (HighInDays - LowInDays)
+    PercentK = PeriodRSV.ewm(span=3).mean()
+    PercentD = PercentK.ewm(span=3).mean()
+    PercentJ = 3 * PercentK - 2 * PercentD
+    axes[4].plot(numpy_data.index, PercentK, 'y', label='%K')
+    axes[4].plot(numpy_data.index, PercentD, 'c', label='%D')
+    axes[4].plot(numpy_data.index, PercentJ, 'k', label='%J')
 
     plt.savefig(image_link, dpi=100)
     plt.close('all')

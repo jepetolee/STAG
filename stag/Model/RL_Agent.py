464d643ad02ae666d5ec1518cf31309a5b914b6f
jepetolee <55366212+jepetolee@users.noreply.github.com>
2022-10-18 17:43:02 +0900

    building virtual reward env

--- stag/Model/RL_Agent.py
+++ stag/Model/RL_Agent.py
@@ -16,6 +16,10 @@ LEVERAGE_HIGH = 5
 LEVERAGE_DEFAULT = 3
 LEVERAGE_LOW = 1
 
+# ACTION_CHANGED
+UNSTARTED = 3
+POSITION_UNCHANGED = 4
+POSITION_CHANGED = 5
 
 # CRYPTOTYPES
 BTC_DECIMAL_POINT = 1000
@@ -24,9 +28,21 @@ ETH_DECIMAL_POINT = 1
 THERES_NO_CRYPTO = 'NONE'
 
 
+def ChangePosition2Integer(position):
+    if position is POSITION_HOLD:
+        return 0
+    elif position is POSITION_LONG:
+        return 1
+    elif position is POSITION_SHORT:
+        return 2
+    else:
+        print("It has error to decide Position")
+
+
 class RL_Agent:
     def __init__(self, leverage, testmode=true):
         self.Agent = FutureTrader()
+        self.CheckActionChanged = UNSTARTED
         self.RealTrader = self.Agent.Trader
         self.IsTestMode = testmode
         self.Leverage = leverage
@@ -42,16 +58,13 @@ class RL_Agent:
         self.CurrentCallingSize = 0
         self.CurrentCryptoName = THERES_NO_CRYPTO
 
-    def Trade(self, crypto_name, position, crypto_decimal_points):
+    def Trade(self, crypto_name, crypto_decimal_points):
         self.CurrentCryptoName = crypto_name
 
         self.PositionPrice = FutureTrader.CurrentPrice(self.CurrentCryptoName)
-
-        self.CurrentPosition = position
         self.TradeCounts += 1
 
         if not self.IsTestMode:
-
             self.CurrentCallingSize = self.count_token_size(self.Leverage, self.Agent.CallableUsdt(),
                                                             crypto_decimal_points)
             self.RealTrader.futures_create_order(symbol=self.CurrentCryptoName, type='LIMIT', timeInForce='GTC',
@@ -81,3 +94,35 @@ class RL_Agent:
         calling_size = int(floored_size / crypto_decimal_points)
 
         return float(calling_size)
+
+    def get_reward(self):
+        return
+
+    def check_price_type(self, price):
+        if self.CheckActionChanged is POSITION_CHANGED:
+            self.PositionPrice = price
+            self.CheckActionChanged = POSITION_UNCHANGED
+        else: pass
+        self.CurrentPrice = price
+
+    def check_position(self, action):
+        if self.CheckActionChanged is UNSTARTED:
+            self.CheckActionChanged = POSITION_CHANGED
+            return self.decide_position(action)
+        elif ChangePosition2Integer(self.CurrentPosition) == action.item():
+            self.CheckActionChanged = POSITION_UNCHANGED
+            return self.CurrentPosition
+        else:
+            self.CheckActionChanged = POSITION_CHANGED
+            return self.decide_position(action)
+
+    def decide_position(self, action):
+        if action.item() == 0:
+            self.CurrentPosition = POSITION_HOLD
+        elif action.item() == 1:
+            self.CurrentPosition = POSITION_LONG
+        elif action.item() == 2:
+            self.CurrentPosition = POSITION_SHORT
+        else:
+            print("there is an error for deciding position")
+        return self.CurrentPosition
--- stag/TrainModelWithDataset.py
+++ stag/TrainModelWithDataset.py
@@ -5,7 +5,7 @@ from DatasetBuilder import *
 from tqdm import trange
 
 
-def Train(crypto_name, device='cpu', train_steps_per_trade=10):
+def TrainWithDataset(crypto_name, device='cpu', train_steps_per_trade=10):
     model = Dreamer(device=device, train_steps=train_steps_per_trade)
     image_data = DatasetBuilder.Dataset(crypto_name)
     testing_number_data = TakeCsvData(detailed_dataset_root(crypto_name, '15m'))
@@ -13,6 +13,9 @@ def Train(crypto_name, device='cpu', train_steps_per_trade=10):
 
     for time_steps in trange(image_data.size):
         crypto_chart = image_data.call_image_tensor(time_steps)
-        average_price = testing_number_data['3'][LeastNumber2Build + time_steps]
         action, action_distribution, value_prediction, reward_prediction, posterior_state = model.RunModel(crypto_chart)
-        
+        close_price_in_csv_data = testing_number_data['3'][LeastNumber2Build + time_steps]
+
+        virtual_trader.check_position(action)
+        virtual_trader.check_price_type(close_price_in_csv_data)
+        reward = virtual_trader.get_reward()
